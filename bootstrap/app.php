4ef2647e573bdd94aa1b17182895ac3329e8764c
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-10-29 22:49:31 +0200

    fix container require path, add twig and error middleware, add first route to home

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,11 +2,18 @@
 
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use FreightQuote\User\UserController;
 
-$container = require './container.php';
+$container = require __DIR__.'/container.php';
 
 $app = AppFactory::createFromContainer($container);
 
 $twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+
+$app->add(TwigMiddleware::create($app, $twig));
+$app->addErrorMiddleware(true, false, false);
+
+$app->get('/', [UserController::class, 'home']);
 
 return $app;
