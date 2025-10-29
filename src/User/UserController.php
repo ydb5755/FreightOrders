0c7d628d15ed81e6ff09062fd4f9ff193bd926ac
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-10-29 22:49:08 +0200

    user controller with home method

---
+++ src/User/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FreightQuote\User;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\RequestInterface as Request;
+use Slim\Views\Twig;
+
+class UserController
+{
+    public function home(Request $request, Response $response): Response
+    {
+        $view = Twig::fromRequest($request);
+
+        return $view->render($response, 'home.html.twig', [
+            'name' => 'name'
+        ]);
+    }
+}
