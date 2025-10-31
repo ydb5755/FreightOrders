2bc3dfc3eccec901a2ce35f2fd799395f7fd7f6d
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-10-31 12:10:33 +0200

    add middleware and use DI-bridge for instantiating app and add routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,30 @@
 <?php
 
-use Slim\Factory\AppFactory;
+use DI\Bridge\Slim\Bridge;
+use Slim\Csrf\Guard as CsrfMiddleware;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 use FreightQuote\User\UserController;
+use FreightQuote\MiddleWare\AuthMiddleware;
+use FreightQuote\MiddleWare\CsrfToTwigMiddleware;
 
 $container = require __DIR__.'/container.php';
-
-$app = AppFactory::createFromContainer($container);
+$app = Bridge::create($container);
 
 $twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
 
+$responseFactory = $app->getResponseFactory();
+$csrf = new CsrfMiddleware($responseFactory);
+$csrfTwig = new CsrfToTwigMiddleware($twig);
+$auth = new AuthMiddleware();
+$app->add($csrf);
+$app->add($csrfTwig);
 $app->add(TwigMiddleware::create($app, $twig));
 $app->addErrorMiddleware(true, false, false);
 
 $app->get('/', [UserController::class, 'home']);
+$app->get('/login', [UserController::class, 'login']);
+$app->post('/login', [UserController::class, 'doLogin']);
+$app->get('/dashboard', [UserController::class, 'dashboard'])->add($auth);
 
 return $app;
