4f89dc5e9449e0ffe69544deac066fa75b3f8e19
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-02 23:12:13 +0200

    add test for failed login

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,11 +1,21 @@
 describe('user login', () => {
-    it('logs in', () => {
+    beforeEach(() => {
         cy.exec('npm run db:seed')
+    })
+    it('logs in', () => {
         cy.visit('/')
         cy.get('a').click()
         cy.url().should('include', '/login')
         cy.get('input[name=email]').type('test@test.com')
         cy.get('input[name=password]').type('password{enter}')
         cy.url().should('include', '/dashboard')
+    })
+    it('doesnt log in', () => {
+        cy.visit('/')
+        cy.get('a').click()
+        cy.url().should('include', '/login')
+        cy.get('input[name=email]').type('test@test.com')
+        cy.get('input[name=password]').type('wrongPassword{enter}')
+        cy.url().should('include', '/login')
     })
 })
