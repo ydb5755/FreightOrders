f69130c02c2dd65e9d140448381339179d7051be
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-02 23:36:53 +0200

    instantiate binding, when used

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -3,9 +3,10 @@
 use DI\Container;
 use FreightQuote\User\FlatFileUserRepository;
 use FreightQuote\User\UserRepository;
+use DI;
 
 $container = new Container([
-    UserRepository::class => FlatFileUserRepository::class,
+    UserRepository::class => DI\create(FlatFileUserRepository::class),
 ]);
 
 return $container;
