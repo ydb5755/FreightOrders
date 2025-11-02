5107210098bbcaa1dfca9ab7c2af19083e12cbd3
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-02 23:07:55 +0200

    test successful login

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('user login', () => {
+    it('logs in', () => {
+        cy.exec('npm run db:seed')
+        cy.visit('/')
+        cy.get('a').click()
+        cy.url().should('include', '/login')
+        cy.get('input[name=email]').type('test@test.com')
+        cy.get('input[name=password]').type('password{enter}')
+        cy.url().should('include', '/dashboard')
+    })
 })
