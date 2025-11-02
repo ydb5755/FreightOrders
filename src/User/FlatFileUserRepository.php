2c2b1751a70534094689b3c493152d1721d48015
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-02 09:57:52 +0200

    implement save for update and new users

--- src/User/FlatFileUserRepository.php
+++ src/User/FlatFileUserRepository.php
@@ -26,5 +26,33 @@ class FlatFileUserRepository implements UserRepository
 
     public function save(User $user): User
     {
+        $data = $this->getUsersData();
+        foreach ($data as $jsonUser) {
+            if ($jsonUser['email'] === $user->getEmail()) {
+                $jsonUser['password'] = $user->getPassword();
+                file_put_contents(
+                    $this->pathToUserFile,
+                    json_encode($data, JSON_PRETTY_PRINT)
+                );
+                return new User(
+                    $jsonUser['email'],
+                    $jsonUser['password']
+                );
+            }
+        }
+        $newUser = [
+            'email' => $user->getEmail(),
+            'password' => $user->getPassword(),
+        ];
+        $data[] = $newUser;
+        file_put_contents(
+            $this->pathToUserFile,
+            json_encode($data, JSON_PRETTY_PRINT)
+        );
+        return new User(
+            $newUser['email'],
+            $newUser['password']
+        );
+        
     }
 }
