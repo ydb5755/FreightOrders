c908381d1ed9919466df5995b9673ab08a085a08
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-02 09:43:27 +0200

    extract getting data to private method

--- src/User/FlatFileUserRepository.php
+++ src/User/FlatFileUserRepository.php
@@ -6,10 +6,17 @@ class FlatFileUserRepository implements UserRepository
 {
     private string $pathToUserFile = __DIR__.'/../../storage/users.json';
 
-    public function findByEmail(string $email): ?User
+    private function getUserData(): array
     {
         $json = file_get_contents($this->pathToUserFile);
         $data = json_decode($json, true);
+
+        return $data;
+    }
+
+    public function findByEmail(string $email): ?User
+    {
+        $data = $this->getUserData();
         foreach ($data as $user) {
             if ($user['email'] === $email) {
                 return new User($user['email'], $user['password']);
