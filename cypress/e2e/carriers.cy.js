2ed0dc1ea65c1366a48a730c791b9c0361c1ef02
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-04 10:23:27 +0200

    tets carrier creation

---
+++ cypress/e2e/carriers.cy.js
@@ -0,0 +1,15 @@
+describe('freight carriers', () => {
+    beforeEach(() => {
+        cy.exec('npm run db:seed')
+    })
+    it('creates freight carrier', () => {
+        cy.visit('/freight_carriers')
+        cy.get('input[name=company]').type('test company')
+        cy.get('input[name=contact_person]').type('Joe Shmoe')
+        cy.get('input[name=email]').type('joe@shmoe.com')
+        cy.get('input[name=phone_number]').type('123456789')
+        cy.get('input[name=notes]').type('some notes{enter}')
+        cy.url().should('include', '/freight_carriers')
+        cy.get('table').contains('test company')
+    })
+})
