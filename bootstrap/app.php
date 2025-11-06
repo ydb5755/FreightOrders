95feab744e0d2af6ffd9fdb96fe4203f311c3ab1
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-06 10:19:33 +0200

    create route for carrier page and post endpoint for creating carrier

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Slim\Csrf\Guard as CsrfMiddleware;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 use FreightQuote\User\UserController;
+use FreightQuote\Carrier\CarrierController;
 use FreightQuote\MiddleWare\AuthMiddleware;
 use FreightQuote\MiddleWare\CsrfToTwigMiddleware;
 
@@ -27,5 +28,7 @@ $app->get('/', [UserController::class, 'home']);
 $app->get('/login', [UserController::class, 'login']);
 $app->post('/login', [UserController::class, 'doLogin']);
 $app->get('/dashboard', [UserController::class, 'dashboard'])->add($auth);
+$app->get('/freight_carriers', [CarrierController::class, 'page']);
+$app->post('/create_carrier', [CarrierController::class, 'create']);
 
 return $app;
