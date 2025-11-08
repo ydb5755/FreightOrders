13c2464f95230b9ba42041374911d01250ddc5d2
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-08 22:56:16 +0200

    add use cases to Carrier Controller
    get all and create carrier

--- src/Carrier/CarrierController.php
+++ src/Carrier/CarrierController.php
@@ -2,20 +2,41 @@
 
 namespace FreightQuote\Carrier;
 
+use FreightQuote\Carrier\UseCases\CreateCarrier;
+use FreightQuote\Carrier\UseCases\CreateCarrierRequest;
+use FreightQuote\Carrier\UseCases\GetAllCarriers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
 class CarrierController
 {
+    public function __construct(
+        private CarrierRepository $carrierRepo,
+    ) {}
+
     public function page(Request $request, Response $response): Response
     {
         $twig = Twig::fromRequest($request);
 
-        return $twig->render($response, 'carriers.html.twig');
+        $useCase = new GetAllCarriers($this->carrierRepo);
+        $result = $useCase->execute();
+
+        return $twig->render($response, 'carriers.html.twig', [
+            'carriers' => $result,
+        ]);
     }
 
     public function create(Request $request, Response $response): Response
     {
+        $body = $request->getParsedBody();
+        $dto = new CreateCarrierRequest(
+            $body['email'],
+            $body['companyName'],
+        );
+        $useCase = new CreateCarrier($dto, $this->carrierRepo);
+        $useCase->execute();
+
+        return $response->withHeader('Location', '/freight_carriers')->withStatus(303);
     }
 }
