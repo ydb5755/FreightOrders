5f3fa3548618c75e138178d2de99e3891ce540d3
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-08 21:26:31 +0200

    bind carrier repo in container

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,12 +1,15 @@
 <?php
 
 use DI\Container;
+use FreightQuote\Carrier\CarrierRepository;
+use FreightQuote\Carrier\FlatFileCarrierRepository;
 use FreightQuote\User\FlatFileUserRepository;
 use FreightQuote\User\UserRepository;
 use DI;
 
 $container = new Container([
     UserRepository::class => DI\create(FlatFileUserRepository::class),
+    CarrierRepository::class => DI\create(FlatFileCarrierRepository::class),
 ]);
 
 return $container;
