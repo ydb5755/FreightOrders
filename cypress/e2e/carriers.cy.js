2e7d67adde1d1977295dc927a3f25403a8bf5d7e
Yisroel Baum <yisroel.d.baum@gmail.com>
2025-11-09 21:02:59 +0200

    add test for no carriers to display

--- cypress/e2e/carriers.cy.js
+++ cypress/e2e/carriers.cy.js
@@ -1,8 +1,6 @@
 describe('freight carriers', () => {
-    beforeEach(() => {
-        cy.exec('npm run db:seed')
-    })
     it('creates freight carrier', () => {
+        cy.exec('npm run db:seed')
         cy.visit('/freight_carriers')
         cy.get('input[name=companyName]').type('test company')
         cy.get('input[name=contact_person]').type('Joe Shmoe')
@@ -11,5 +9,10 @@ describe('freight carriers', () => {
         cy.get('input[name=notes]').type('some notes{enter}')
         cy.url().should('include', '/freight_carriers')
         cy.get('table').contains('test company')
+    })
+    it('shows message if no carriers to display', () => {
+        cy.exec('npm run db:wipe')
+        cy.visit('/freight_carriers')
+        cy.contains('No Carriers to Display')
     })
 })
